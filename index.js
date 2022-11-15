d56cd808feca6fb3eec8f6bc8dccd2c00625f961
benita sien <benitasien@gmail.com>
2022-11-15 19:45:53 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function checkResult(element) {
+    return (element.result === "W")
+}
+function superbowlWin(array) {
+    const result =  array.find(checkResult);
+    return result ? result.year : result;
+}
